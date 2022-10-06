fda9e3f453a0339910de68a7fbf8d966040c04b1
JordanLindley <jlindley94@gmail.com>
2022-10-06 17:39:37 -0400

    Generate 256 divs and style into grid

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const divContainer = document.querySelector('.container');
+const container = document.querySelector('.container');
 
+// area of grid
 const divsInGrid = 256;
 
+// loop up to the area of 
 function populateDivs() {
-  for (let i = 0; i <= divsInGrid; i++) {
-    const gridDiv = document.createElement('div');
-    gridDiv.className = 'grid';
+  for (let i = 0; i < divsInGrid; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('item-' + (i + 1), 'grid');
     // gridDiv.style.cssText = 'display: flex; justify-content: center; width: 32px;'
-    divContainer.appendChild(gridDiv);
+    container.appendChild(cell);
   }
 }
 
 populateDivs();
+
+
